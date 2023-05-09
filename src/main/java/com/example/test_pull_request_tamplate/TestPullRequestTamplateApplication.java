3112b691bd5487a22f22c4fcd8ba7470304c8bc5
Piotr Mackowiak <mackowiak55@gmail.com>
2023-05-10 00:44:27 +0200

    feature: add something to hello text

--- src/main/java/com/example/test_pull_request_tamplate/TestPullRequestTamplateApplication.java
+++ src/main/java/com/example/test_pull_request_tamplate/TestPullRequestTamplateApplication.java
@@ -8,7 +8,7 @@ public class TestPullRequestTamplateApplication{
 
     public static void main(String[] args){
         SpringApplication.run(TestPullRequestTamplateApplication.class, args);
-        System.out.println("hello");
+        System.out.println("hello dawciobiel");
 
     }
 
