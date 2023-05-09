6b0651652c6db92b31900f3e6c65dadab07cfd14
odzio33 <poodzio0604@wp.pl>
2023-05-10 00:16:54 +0200

    Initial commit

---
+++ src/main/java/com/example/test_pull_request_tamplate/TestPullRequestTamplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_pull_request_tamplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPullRequestTamplateApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(TestPullRequestTamplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_pull_request_tamplate/TestPullRequestTamplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_pull_request_tamplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPullRequestTamplateApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
